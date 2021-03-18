4af5bd2eba69850e68cf5901c17c50eedb43723b
rad <cepelka.jr@email.cz>
2021-03-18 16:23:49 +0100

    18-01

--- indicators.py
+++ indicators.py
@@ -6,7 +6,20 @@ def MA(period, data):
 # uptrend -> začne klesat, max - low = firstSupport, max - mid = secondSupport (při otočení zpět do uptrendu signal Buy)
 # exit strategy -> stoploss cca 55 %
 # target profit ->
+
+def write_values(ticker: str, *values) -> None:
+    """
+    Uložení hodnot z obchodování do souboru
+
+    :param ticker: Kód akcie
+    :param values: hodnoty z obchodování po zavření trhu
+    """
+    pass
+
 def fibonaci(min: float, max: float) -> list:
+    """
+    SMA period 20
+    """
     price_range = max - min
     mid = min + price_range / 2
     low = min + price_range / 100 * 38.2
@@ -39,14 +52,15 @@ def RSI_14(value_yesterday: float, value_today: float) -> str:
         result = "trend will turn up "
     return result
 
-# všechny hodnoty z PSE
+# všechny hodnoty brány z PSE
 
 # 16.3.21, BAACEZ, close=537,
-# RSI_14=68.86 %,
+# RSI_14=68.86 %
+# BB, top=543, mid=527, low=512
 
 # 17.3.21, BAACEZ, close=541, min=538, max=544, obj_tis=129,04, RMS, min=535, max=545, close=543
 # RSI_14=71.38 %
-# BB, top=550, mid=532, low=514
+# BB, top=545, mid=527, low=510
 
 
 print(f"fib:", fibonaci(535, 545))
@@ -55,3 +69,4 @@ print(f"fib:", fibonaci(535, 545))
 
 
 print(f"RSI:", RSI_14(68.86, 71.38))
+
